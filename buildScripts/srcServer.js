eb15bba6ad84791bf0a18e544a6cdb2f2090ce7c
Neeru Verma <neeru.verma@nagarro.com>
2017-05-19 15:03:31 +0530

    Updated files

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
 var app = express();
 var port = 3000;
 app.get('/',function(req,res){
